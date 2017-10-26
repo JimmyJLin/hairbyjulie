e75bf26ca3add8736e7cf7052168402a868be161
Jimmy Lin <jimmylin@Jimmys-MacBook-Pro.local>
2017-10-26 12:12:29 -0400

    refactored the react code for basic outline to javascript

--- routes/main.js
+++ routes/main.js
@@ -8,6 +8,26 @@ main.get('/', (req, res) => {
   res.render('pages/index');
 });
 
+main.get('/services', (req, res) => {
+  res.render('pages/services');
+});
+
+main.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+main.get('/review', (req, res) => {
+  res.render('pages/review');
+});
+
+main.get('/contact', (req, res) => {
+  res.render('pages/contact');
+});
+
+main.get('/appointment', (req, res) => {
+  res.render('pages/appointment');
+});
+
 main.get('/*', (req, res) => {
   res.render('pages/404');
 });
